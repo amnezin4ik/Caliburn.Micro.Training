bf25ead10dd2e9b377c3eb5ebffe856b234634ef
amnezin <alexei.zhukov@softeq.com>
2017-02-12 16:48:16 +0300

    Test for observable collection binding

--- Caliburn.Micro.Training.Application/ViewModels/MainWindow/MainWindowViewModel.cs
+++ Caliburn.Micro.Training.Application/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -1,12 +1,20 @@
+using System.Collections.ObjectModel;
 using Caliburn.Micro.Training.Application.Validation;
+using Caliburn.Micro.Training.Domain;
 
 namespace Caliburn.Micro.Training.Application.ViewModels.MainWindow
 {
     public class MainWindowViewModel : BaseValidatedViewModel
     {
-        public MainWindowViewModel(MainWindowValidator validator) 
+        public MainWindowViewModel(MainWindowValidator validator)
             : base(validator)
         {
+            UsersCollection = new ObservableCollection<User>
+            {
+                new User {Name = "Stephen"},
+                new User {Name = "Bill"},
+                new User {Name = "John"}
+            };
         }
 
         private string _name;
@@ -19,5 +27,7 @@ namespace Caliburn.Micro.Training.Application.ViewModels.MainWindow
                 NotifyOfPropertyChange(nameof(Name));
             }
         }
+
+        public ObservableCollection<User> UsersCollection { get; private set; }
     }
 }
---
+++ Caliburn.Micro.Training.Domain/User.cs
@@ -0,0 +1,7 @@
+namespace Caliburn.Micro.Training.Domain
+{
+    public sealed class User
+    {
+        public string Name { get; set; }
+    }
+}
