98b217a59588e6b7206ccb277f15b083959d7bb6
amnezin <alexei.zhukov@softeq.com>
2017-02-20 10:36:12 +0300

    remove extra code

--- Caliburn.Micro.Training.Wpf/AppBootstrapper.cs
+++ Caliburn.Micro.Training.Wpf/AppBootstrapper.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
 using System.Windows;
 using Autofac;
 using AutoMapper;
 using Caliburn.Micro.Autofac;
 using Caliburn.Micro.Training.Domain;
 using Caliburn.Micro.Training.Domain.Services;
-using Caliburn.Micro.Training.Domain.Services.Services;
 using Caliburn.Micro.Training.Infrastructure;
 using Caliburn.Micro.Training.Wpf.ViewModels;
 
@@ -20,11 +18,6 @@ namespace Caliburn.Micro.Training.Wpf
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            //var viewModelSettings = new Dictionary<string, object>();
-            //var userService = (IUserService)Container.Resolve(typeof (IUserService));
-            //var currentUser = userService.FindUser(1).Result;
-            //viewModelSettings.Add("currentUser", currentUser);
-            //DisplayRootViewFor<MainWindowViewModel>(viewModelSettings);
             DisplayRootViewFor<MainWindowViewModel>();
         }
 
